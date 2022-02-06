db3f4c19e916d26ca0d880ce37acff823865e025
Vasanth Developer <vsnthdev@gmail.com>
2022-02-06 16:55:31 +0530

    aligned about section text to center

--- src/About.tsx
+++ src/About.tsx
@@ -4,11 +4,10 @@ function about() {
 	return (
 		<div id="About" class=" bg-black text-white flex justify-center items-center h-screen pb-10">
 			<div class="  flex justify-between px-10  w-full ">
-		    
 			<div>
 				<h1 class="mx-28 text-6xl font-mono">About Us</h1>
 			</div>
-			<div>
+			<div className="flex justify-center">
 				<p class="w-1/2 font-mono">
 					Lorem gejh hgehge hfehghe hequgfu ewiqih hwhuhgur uwehurgwbu ubfuwuefuu
 				</p>
