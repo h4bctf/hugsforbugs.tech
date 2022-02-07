402923d2cb3d0f8dac61dfde465e9c62f509dbc5
navjotsingh1234 <navjotsingh301103@gmail.com>
2022-02-07 18:46:46 +0530

    modification

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -3,7 +3,7 @@ import react from "React";
 function banner() {
 	return (
 		<div id="Home" class="flex justify-center items-center w-full h-full bg-[#0A0A0A]">
-			<img class="w-full  mx-28 text-8xl m-40 font-mono" src="https://readme-typing-svg.herokuapp.com?color=%23F7F7F7&size=25&lines=Pwn%E2%80%99in%E2%80%99+time;+Pwn+o%E2%80%99Clock" alt=""/>
+			<img class="w-full  mx-28 text-8xl m-40 font-mono" src="https://readme-typing-svg.herokuapp.com?color=%23FFFFFF&size=25&lines=IT'S+PWN'ING+TIME;PWN+O'+CLOCK" alt=""/>
 	     	
 		</div>
 
