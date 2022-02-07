0f46816853f7e6a8d3d0f92738fc3ca61c7213ab
navjotsingh1234 <navjotsingh301103@gmail.com>
2022-02-07 20:46:06 +0530

    class to className

--- src/Team.tsx
+++ src/Team.tsx
@@ -4,9 +4,9 @@ function team() {
 	return (
 		<div
 			id="Team"
-			class="bg-[#0A0A0A] text-white flex  justify-center items-center"
+			className="bg-[#0A0A0A] text-white flex  justify-center items-center"
 		>
-			<h1 class="flex text-8xl font-mono pt-12">Team</h1>
+			<h1 className="flex text-8xl font-mono pt-12">Team</h1>
 		</div>
 	);
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,8 +2,8 @@ import react from "React";
 
 const banner: React.FC =()=> {
 	return (
-		<div id="Home" class="flex justify-center items-center w-full h-full bg-[#0A0A0A]">
-			<img class="w-full  mx-28 text-8xl m-40 font-mono" src="https://readme-typing-svg.herokuapp.com?color=%23FFFFFF&size=25&lines=IT'S+PWN'ING+TIME;PWN+O'+CLOCK" alt=""/>
+		<div id="Home" className="flex justify-center items-center w-full h-full bg-[#0A0A0A]">
+			<img className="w-full  mx-28 text-8xl m-40 font-mono" src="https://readme-typing-svg.herokuapp.com?color=%23FFFFFF&size=25&lines=IT'S+PWN'ING+TIME;PWN+O'+CLOCK" alt=""/>
 	     	
 		</div>
 	);
