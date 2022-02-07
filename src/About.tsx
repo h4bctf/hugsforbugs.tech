71fe25ea146fe24dd7b72db0b633291c67578b81
navjotsingh1234 <navjotsingh301103@gmail.com>
2022-02-07 13:13:23 +0530

    Modified about section

--- src/About.tsx
+++ src/About.tsx
@@ -2,20 +2,30 @@ import react from "React";
 
 function about() {
 	return (
-		<div id="About" class=" bg-black text-white flex justify-center items-center h-screen pb-10">
+		<div
+			id="About"
+			class=" bg-black text-white flex justify-center items-center h-screen pb-10"
+		>
 			<div class="  flex justify-between px-10  w-full ">
-			<div>
-				<h1 class="mx-28 text-6xl font-mono">About Us</h1>
-			</div>
-			<div className="flex justify-center">
-				<p class="w-1/2 font-mono">
-					Lorem gejh hgehge hfehghe hequgfu ewiqih hwhuhgur uwehurgwbu ubfuwuefuu
-				</p>
+				<div>
+					<h1 class="mx-28 text-6xl font-mono flex">About Us</h1>
+				</div>
+				<div className=" flex justify-center">
+					<p class="w-1/2 font-mono">
+						HugsForBugs is an international community where we play
+						CTFs and learn things together. We play around once or
+						twice a month or so and aren't super hardcore. No one
+						will expect you to stay up all night two nights in a
+						row! Our members range from high school students to
+						established cybersecurity professionals, complete n00bs
+						to professional pen testers. We also hang out, discuss
+						the latest 0-day in the news, show off our latest bug
+						bounty, and share silly memes.
+						
+					</p>
+				</div>
 			</div>
 		</div>
-
-		</div>
-		
 	);
 }
 
