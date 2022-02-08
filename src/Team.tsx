adedd46a50f9f80a43abacdb698c01d5123b7015
navjotsingh1234 <navjotsingh301103@gmail.com>
2022-02-08 12:32:34 +0530

    Partial-deploy

--- src/Team.tsx
+++ src/Team.tsx
@@ -6,7 +6,7 @@ function team() {
 			id="Team"
 			className="bg-[#0A0A0A] text-white flex  justify-center items-center"
 		>
-			<h1 className="flex text-8xl font-mono pt-12">Tea</h1>
+			<h1 className="flex text-8xl font-mono pt-12">Team</h1>
 		</div>
 	);
 }
