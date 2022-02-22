67c9fa6b96a7ec68259ad041edf46d4f7af4ae3c
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2022-02-22 22:24:15 +0530

    New branch for testing Purpose and for better management

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,13 @@ import React from 'react'
 	  <>
 	   <div className="w-full items-center flex justify-between px-10 bg-[#0A0A0A] h-20 fixed top-0 bg-opacity-20  backdrop-blur-md">
       <div>
+      <a href="">
         <img
           className="
            w-20"
           src="https://ctftime.org/media/cache/8d/37/8d3746b689ba3c133cd3599d898b6a4b.png"
           alt="H4B"
-        />
+        />  </a> 
       </div>
 
       <div className="w-1/3">
