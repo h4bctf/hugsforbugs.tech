a57edcc688ded3cf35cc15f90f93b7eae56a3619
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2022-02-27 09:37:55 +0530

    being done with about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,40 +1,36 @@
 import React from "react";
 import "./styles/About.scss";
+import AboutIllust from "../assets/undraw_software_engineer_re_fyew.svg";
 
 const About: React.FC = () => {
   return (
-    <div id="About" className="h-screen w-full bg-[#11121A]">
-      {/* <div className="  flex justify-between px-10  w-full ">
-				<div>
-					<h1 className="mx-36 text-6xl font-mono flex mr">About Us</h1>
-				</div>
-				<div className=" flex justify-center">
-					<p className="container w-1/2 font-mono mx-28 flex-auto">
-						HugsForBugs is an international community where we compete in
-						CTFs and learn things together. We play around once or
-						twice a month or so and aren't super hardcore. No one
-						will expect you to stay up all night two nights in a
-						row! Our members range from high school students to
-						established cybersecurity professionals, complete n00bs
-						to professional pen testers. We also hang out, discuss
-						the latest 0-day in the news, show off our latest bug
-						bounty, and share silly memes.
-					</p>
-				</div>
-			</div> */}
-      <div className="w-full text-center ">
-        <h1 className="font-['Poppins'] text-[#fff]">About Us</h1>
+    <div id="About" className="h-screen w-full bg-[#11121A] flex flex-col justify-between ">
+      <div className="pt-4 flex flex-col justify-evenly align-center h-3/5 sm:flex-row sm:h-2/5">
+        <div className="w-full text-center sm:w-1/4">
+          <h1 className="font-['Poppins'] text-[#fff] text-4xl pt-3">
+            About Us
+          </h1>
+        </div>
+        <div className=" flex justify-center w-full sm:w-1/2 overflow-hidden">
+          <p className="text-[#d3d3d3] font-['Poppins'] p-4 text-left text-md sm:text-lg">
+            HugsForBugs is an international{" "}
+            <span className="underline decoration-red-600">community </span>{" "}
+            where we compete in{" "}
+            <span className="underline decoration-purple-600"> CTFs </span> and
+            learn things together. We play around once or twice a month or so
+            and aren't super hardcore. No one will expect you to stay up all
+            night two nights in a row! Our{" "}
+            <span className="underline decoration-blue-600"> members </span>{" "}
+            range from high school students to established <span className="underline decoration-orange-500"> cybersecurity
+            professionals </span>, complete{" "}
+            <span className="underline decoration-lime-500"> n00bs </span> to
+            professional pen testers. We also hang out, discuss the latest 0-day
+            in the news, show off our latest bug bounty, and share silly memes.
+          </p>
+        </div>
       </div>
-      <div className="flex justify-center ">
-        <p className="text-[#d3d3d3] p-4 text-left ">
-          HugsForBugs is an international <span className="underline decoration-red-600">community </span> where we compete in CTFs and
-          learn things together. We play around once or twice a month or so and
-          aren't super hardcore. No one will expect you to stay up all night two
-          nights in a row! Our <span className="underline decoration-blue-600"> members </span> range from high school students to
-          established cybersecurity professionals, complete <span className="underline decoration-lime-500"> n00bs </span> to
-          professional pen testers. We also hang out, discuss the latest 0-day
-          in the news, show off our latest bug bounty, and share silly memes.
-        </p>
+      <div className="w-full h-1/3  flex justify-center   p-4 sm:h-1/2">
+        <img src={AboutIllust} alt="" className="h-auto w-full max-w-full " />
       </div>
     </div>
   );
