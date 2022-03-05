f602bc4c8054e0a3dcadef8e9687ec762b827618
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2022-03-05 10:49:14 +0530

    Animation Created in Cards

--- src/components/Team/Profile.tsx
+++ src/components/Team/Profile.tsx
@@ -1,13 +1,21 @@
 import { MutableRefObject, useEffect ,useRef } from 'react';
+//@ts-ignore
+import VanillaTilt from 'vanilla-tilt';
+
 
 const Profile = () => {
+	const TiltingCard = useRef() as MutableRefObject<HTMLDivElement>;
+
+	useEffect(()=>{
+		VanillaTilt.init(TiltingCard.current , {scale:0.9 , glare:true , perspective:500 , speed:1000 , reverse:true})
+	},[])
+	 
   return (
 	 <>
-	 
-           <div  id="#idk"  className="h-[20rem] sm:h-[22rem] w-10/12 bg-gray-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-50 ">
+           <div  id="#idk" ref={TiltingCard}  className="h-[20rem] sm:h-[22rem] w-10/12 bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-50 ">
 		  
 	   </div>
-	
+
 	 </>
   )
 }
